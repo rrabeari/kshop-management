2f33fdd625221d8bb9b3bd0262f29a62db4fe6dc
Kalim Rabearison <kalimrazah2@gmail.com>
2026-08-06 10:23:37 +0300

    Add files via upload

---
+++ backend/kshop-backend/src/test/java/com/kshop/backend/KshopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kshop.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KshopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
