50b660f7c6daf61eed863bb006358ef9e05ace1c
Kalim Rabearison <kalimrazah2@gmail.com>
2026-08-06 09:40:30 +0300

    Add files via upload

---
+++ backend/kshop-backend/src/main/java/com/kshop/backend/KshopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kshop.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KshopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KshopBackendApplication.class, args);
+	}
+
+}
