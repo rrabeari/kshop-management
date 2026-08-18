2d50650b39840863c4f4786fa4725fd2f2567273
Kalim Rabearison <kalimrazah2@gmail.com>
2026-08-18 13:08:57 +0300

    link fronend

---
+++ backend/kshop-backend/src/main/java/com/kshop/backend/config/CorsConfig.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.kshop.backend.config;
+
+/**
+ *
+ * @author Iris-PC
+ */
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://kshop-management.vercel.app/", "http://localhost:4200") // Remplace par ton URL Vercel exacte
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
--- frontend/kshop-frontend/src/app/core/services/api.config.ts
+++ frontend/kshop-frontend/src/app/core/services/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-  baseUrl: 'http://localhost:8080/api'
+  // // baseUrl: 'http://localhost:8080/api'
+  baseUrl: 'https://kshop-management.onrender.com/api'
 };
